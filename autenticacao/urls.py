47bb867cb67f22a5b2f63efbcf94e9ba179611d9
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-05 23:14:58 +0100

    Configurado login do usuário

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('reset/', views.reset_senha, name='reset'),
+    path('logout/', views.logout, name='logout'),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib import auth
 
 
 def login(request):
     if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'autenticacao/login.html')
     
-    if request.method == 'POST':
-        return redirect(request, 'post/lista_post.html')
+    elif request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('password')
+        
+        
+        user = auth.authenticate(
+            username = usuario,
+            password=senha,
+        )
+        
+        if not user:
+            return redirect('/auth/login')
+        else:
+            auth.login(request, user)
+            return redirect('/create')
     
-
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/auth/login')
+    
+    
 def reset_senha(request):
     return render(request, 'autenticacao/reset.html')
