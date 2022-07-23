67358542546ca61f05a54510343d1b1fd4dca47c
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-23 12:09:04 +0100

    Configurando templates

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -8,37 +8,41 @@ from django.contrib.auth.models import User
 def login(request):
     if request.method == 'GET':
         if request.user.is_authenticated:
-            messages.add_message(request, constants.ERROR, 'Você já está logado.')
+            messages.add_message(request, constants.ERROR,
+                                 'Você já está logado.')
             return redirect('/')
         return render(request, 'autenticacao/login.html')
-    
+
     elif request.method == 'POST':
         usuario = request.POST.get('usuario')
         senha = request.POST.get('password')
-        
-        
+
         user = auth.authenticate(
-            username = usuario,
+            username=usuario,
             password=senha,
         )
-        
+
         if not user:
-            messages.add_message(request, constants.ERROR, 'Usuário não cadastrado.')
+            messages.add_message(request, constants.ERROR,
+                                 'Usuário não cadastrado.')
             return redirect('/auth/login')
         else:
             auth.login(request, user)
             return redirect('/create')
-    
-    
+
+
 def logout(request):
     auth.logout(request)
-    messages.add_message(request, constants.SUCCESS, 'Logout realizado com sucesso.')
+    messages.add_message(request, constants.SUCCESS,
+                         'Logout realizado com sucesso.')
     return redirect('/auth/login')
-    
+
 # TODO configurar o reset de senha
+
+
 def reset_senha(request):
     email = request.POST.get('email')
     user = User.objects.filter(email=email)
-    
+
     print(user)
     return render(request, 'autenticacao/reset.html')
--- post/views.py
+++ post/views.py
@@ -6,16 +6,16 @@ def post_create(request):
     if request.method == 'POST':
         titulo = request.POST.get('titulo')
         descricao_curta = request.POST.get('descricao_curta')
-        descricao_longa = request.POST.get('descricao_longa')    
-        
+        descricao_longa = request.POST.get('descricao_longa')
+
         novo_post = CreatePost(
             titulo=titulo,
             descricao_curta=descricao_curta,
             descricao_longa=descricao_longa,
         )
-        
+
         novo_post.save()
-    
+
     return render(request, 'post/create_post.html')
 
 
